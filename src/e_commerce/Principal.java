b888f670b7b55e8193a94c14e156a6547eb025ae
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2020-09-28 02:19:15 -0300

    Realizado a adição das imagens dos teste de aceitação, rejeição e cancelamento dos pedidos

--- src/e_commerce/Principal.java
+++ src/e_commerce/Principal.java
@@ -8,24 +8,14 @@ public class Principal {
 		Pedido pedido01 = new Pedido();		
 		
 		pedido01.iniciarPedidoNovo();
-		//Tempo máximo para cancelamento de pedido é 14
-		pedido01.fazerAprovacaoDePedido(17);
+		//Tempo máximo para cancelamento de pedido é 14 min
+		pedido01.fazerAprovacaoDePedido(14);
 		pedido01.informarPedidoEmTransporte();
 		pedido01.fazerEntregaDePedito();
 		//OP: 1 p/ Pedido entregue Errado e OP: 02 p/ Pedido OK
 		pedido01.verificarPedidoEntregue(2);
 		pedido01.finalizarPedido();
 		
-//		System.out.println("\n");
-//		//Pedido 02
-//		Pedido pedido02 = new Pedido();		
-//				
-//		pedido02.iniciarPedidoNovo();
-//		pedido02.fazerAprovacaoDePedido(20);
-//		pedido02.informarPedidoEmTransporte();
-//		pedido02.fazerEntregaDePedito(1);
-//		pedido02.verificarPedidoEntregue();
-//		pedido02.finalizarPedido();
 
 	}
 
